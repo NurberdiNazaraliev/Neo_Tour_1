5df6c642d23d132e5e6afe70de68f947ec7ff2a9
Nurberdi <nazaraliev.nurberdi@gmail.com>
2024-03-03 23:10:05 +0100

    chn3

--- TravelApp/settings.py
+++ TravelApp/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Neo_Tour',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'postgres_password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
